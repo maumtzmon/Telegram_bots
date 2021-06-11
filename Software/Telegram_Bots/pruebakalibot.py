17c000d243813a008920d3cff487f1918055000d
maumtzmon <glove1106@gmail.com>
2021-06-11 15:25:16 -0500

    send messages with sugestions from users to me

--- Software/Telegram_Bots/pruebakalibot.py
+++ Software/Telegram_Bots/pruebakalibot.py
@@ -1,7 +1,9 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 from telegram import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext.messagehandler import MessageHandler
-import logging
+
+import logging, os, signal, time
+
 from funciones import funcion_externa
 
 Token_Telegram='1875550428:AAHYqucTFw9z7GF8chxiUIJliBVOBSsmFA8' #token generado por el Bot Father, ver @BotFather en telegram
@@ -11,7 +13,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(mesage)s'
 logger = logging.getLogger('Kalib Bot')
 
 #Variables y archivos que va a utilizar para sus procesos
-
+door = False
 
 #Funciones para que el bot funcione
 
@@ -20,7 +22,7 @@ def start(update, context):
     logger.info('He recibido un comando start')
     name = update.effective_chat.first_name #Se obtiene el nombre del usario.
     chat_id = update.effective_chat.id #obtenemos el Chat ID a donde se andara el mensaje
-    text = "Hola "+ name +" que gusto!\nEstas probando un bot de telegram. Intenta presionar cualquiera de los Botones. \n  Chat Id="+str(chat_id)
+    text = "Hola "+ name +" que gusto!\nEstas probando un bot de telegram. Intenta presionar cualquiera de los Botones. \n" #  Chat Id="+str(chat_id)
     keyboard(chat_id, text, context) #Se envía el mensaje y sale el comando.
     return 0
     
@@ -29,9 +31,10 @@ def keyboard(chat_id, text, context):
     kb = [[KeyboardButton("/Boton_1")], \
          [KeyboardButton("/Boton_2")], \
          [KeyboardButton("/Boton_3")], \
-         [KeyboardButton("/Boton_4a"), KeyboardButton("/Boton_4b")], \
-         [KeyboardButton("/Boton_5a"), KeyboardButton("/Boton_5b")], \
-         [KeyboardButton("/Boton_6")]]
+         #[KeyboardButton("/Boton_4a"), KeyboardButton("/Boton_4b")], \
+         #[KeyboardButton("/Boton_5a"), KeyboardButton("/Boton_5b")], \
+         #[KeyboardButton("/Boton_6")]]
+        ]
     kb1 = ReplyKeyboardMarkup(kb, resize_keyboard=True, one_time_keyboard=True)
     context.bot.send_message(chat_id, text, reply_markup=kb1)
     return 0
@@ -47,7 +50,8 @@ def boton1(update, context):
     keyboard(chat_id, text, context)
     return 0
 
-def boton2(update, context):
+def boton2(update,
+ context):
     logger.info('He recibido un comando Boton 2')
     text = 'Este es un ejemplo usando una funcion que devuelve una tabla usando Pandas, en donde tenemos renglones (0,1,2) y columnas (a,b,c).\n\n' \
         + funcion_externa()
@@ -56,12 +60,58 @@ def boton2(update, context):
     return 0
 
 def boton3(update, context):
-    #intentar enviar imagen de matplotlib  
+    #intentar enviar imagen de matplotlib
+    global door   
+    
     logger.info('He recibido un comando Boton 3')
     text = "sigo pensando aun que podemos hacer en este boton. Que se te ocurre, escribelo abajo ;)"
     chat_id = update.effective_chat.id
-    keyboard(chat_id, text, context)   
-    return 0
+    keyboard(chat_id, text, context)
+    door = True
+
+    #query = update.callback_query
+    #query.answer()
+
+    #return 0
+
+def Text(update,context):
+    global door
+
+    logger.info('recibí mensaje, estoy en text')
+    chat_id = update.effective_chat.id
+    name = update.effective_chat.first_name
+    link = update.effective_chat.link.split('/')[-1]
+    
+    if door == True:
+
+        text = "Perfecto, recibi tu mensaje"
+        
+        context.bot.send_message(chat_id, text)
+
+        messageRecived = update.message.text
+
+        text = 'El mensaje recibido fue: \n-' + messageRecived
+
+        context.bot.send_message(chat_id,text)
+        
+        chat_id_to_Mau = 18616105
+        text_to_Mau = 'el usuario: ' + name + '/ @'+ link + ', hizo una sugerencia para implementar: \n'+ messageRecived
+        context.bot.send_message(chat_id_to_Mau,text_to_Mau)
+    
+    else:
+
+        text = "Que gusto " + name + ", Soy el bot Kali! quieres intentar usar los botones para conocer las opciones de este bot"
+        context.bot.send_message(chat_id, text)
+        
+        
+        chat_id_to_Mau = 18616105
+        messageRecived = update.message.text
+        text_to_Mau = 'el usuario: @' + link + ' / ' + name + ', hizo una sugerencia para implementar: \n'+ messageRecived + '\n '
+        context.bot.send_message(chat_id_to_Mau,text_to_Mau)
+        
+
+
+
 
 if __name__ == '__main__':
 
@@ -72,10 +122,13 @@ if __name__ == '__main__':
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(CommandHandler('Boton_1', boton1))
     dispatcher.add_handler(CommandHandler('Boton_2', boton2))
+    dispatcher.add_handler(CommandHandler('Boton_3', boton3))
     # dispatcher.add_handler(CommandHandler())
     # dispatcher.add_handler(CommandHandler())
     # dispatcher.add_handler(CommandHandler())
-    # dispatcher.add_handler(CommandHandler())
+    
+    #maneja la recepcion de texto por parte de un remitente y Text() maneja ese mensaje
+    dispatcher.add_handler(MessageHandler(Filters.text, Text))
 
 
     updater.start_polling()
